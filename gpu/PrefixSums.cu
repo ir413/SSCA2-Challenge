6a3ca939f9895d77c414edd8d318cffb1019822a
ir413 <ilija.radosavovic@gmail.com>
2016-02-18 23:34:40 +0000

    [prefixsums] Add skeleton for prefixsums

---
+++ gpu/PrefixSums.cu
@@ -0,0 +1,16 @@
+#include "PrefixSums.h"
+
+
+__global__ void prefixSumsKernel(int *data, int *sums, size_t size)
+{
+  (void*) data;
+  (void*) sums;
+  (void*) size;
+}
+
+void prefixSums(int *data, int *sums, size_t size)
+{
+  (void*) data;
+  (void*) sums;
+  (void*) size;
+}
---
+++ gpu/PrefixSums.h
@@ -0,0 +1,9 @@
+#ifndef GPU_PREFIX_SUMS_H
+#define GPU_PREFIX_SUMs_H
+
+/**
+ * Computes the prefix sums of the data.
+ */
+void prefixSums(int *data, int *sums, size_t size); 
+
+#endif // gpu/PrefixSums.h
