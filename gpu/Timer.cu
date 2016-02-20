2e0c059e7a696aad2e73deed5428c4a4f9517b3e
ir413 <ilija.radosavovic@gmail.com>
2016-02-20 22:19:53 +0000

    [time] Get seconds from timeofday

---
+++ gpu/Timer.cu
@@ -0,0 +1,11 @@
+#include <sys/time.h>
+
+#include "Timer.h"
+
+
+double get_seconds()
+{
+  struct timeval tp;
+  gettimeofday(&tp, NULL);
+  return (double) (tp.tv_sec + ((1e-6) * tp.tv_usec));
+}
---
+++ gpu/Timer.h
@@ -0,0 +1,6 @@
+#ifndef GPU_TIMER_H
+#define GPU_TIMER_H
+
+double get_seconds(void);
+
+#endif // gpu/Timer.h 
